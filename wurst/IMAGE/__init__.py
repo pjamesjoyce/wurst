090bf466e0f6e58f6c937daafb745d763835451e
pjamesjoyce <pjamesjoyce@gmail.com>
2019-12-13 11:22:14 +0100

    remove non-python files from wurst package

--- wurst/IMAGE/__init__.py
+++ wurst/IMAGE/__init__.py
@@ -1,10 +1,17 @@
 import json
 import os
+import io as py_io
+import base64
 
 from .io import load_image_data_file
 
-IMAGE_TOPOLOGY = json.load(open(os.path.join(
-    os.path.dirname(__file__),
-    "metadata",
-    "region-topolgy.json"
-)))
+from .metadata import region_topology
+
+
+def _to_filelike(bytestring):
+    return py_io.BytesIO(base64.b64decode(bytestring))
+
+
+IMAGE_TOPOLOGY = json.load(
+    _to_filelike(region_topology)
+)
